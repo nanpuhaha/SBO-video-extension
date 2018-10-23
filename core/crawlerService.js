6d580f4181a838c28e50da5277606ce933075ed6
Jangwon Seo <nanpuhaha@gmail.com>
2018-10-23 15:13:04 +0900

    Overwrite codes with version 2.6 (updated 2018-07-11) from Chrome Web Store

---
+++ core/crawlerService.js
@@ -0,0 +1,68 @@
+var sboModule = sboModule || {};
+
+sboModule.crawlerService = function () {
+
+    let getVideoForRefList = function (apiParams, domList) {
+        apiParams.refList.forEach(function (e, i) {
+            getVideoForRefId(apiParams, domList, e, i);
+        });
+    };
+
+    let getVideoForRefId = function (apiParams, domList, referenceId, index) {
+        let url = 'https://cdnapisec.kaltura.com/html5/html5lib/v2.35/mwEmbedFrame.php?&wid=_' + apiParams.wid +
+            '&uiconf_id=' + apiParams.uiconfId + '&flashvars[referenceId]=' + referenceId + '&callback=o';
+
+        $.get(url).always(function (o) {
+            let frm = o.responseText.indexOf('downloadUrl') + 'downloadUrl'.length + 5;
+            let to = o.responseText.indexOf('",', frm);
+            let url = o.responseText.substring(frm, to).split('\\').join('');
+
+            sboModule.drawService.draw(domList, url, index, extractFlavors(o.responseText));
+        });
+    };
+
+    let extractFlavors = function (responseText) {
+        let frm = responseText.indexOf('flavorAssets') + 'flavorAssets'.length + 3;
+        let to = responseText.indexOf('}],\\"', frm) + 2;
+        let flavorAssets = JSON.parse(responseText.substring(frm, to).split('\\').join(''));
+
+        return flavorAssets.map(it => ({
+            id: it.flavorParamsId,
+            isAudio: it.width === 0 && it.height === 0,
+            width: it.width,
+            height: it.height,
+            bitrate: it.bitrate
+        })).sort((a, b) => (a.width * a.height * a.bitrate) >
+            (b.width * b.height * b.bitrate));
+    };
+
+    return {
+        crawl: function (domList, html) {
+
+            let isNew = !!html;
+            html = isNew ? '<div>' + html + '</div>' : window.document;
+
+            let jsSrc = $('#metadata_flashactive > div > div.sdwBoxContent > div.brightcove_video > script', html).attr('src');
+            if (!jsSrc) {
+                console.error('SBO Downloader is unable to download the course, it is out of control issue.');
+                return;
+            }
+
+            let a = jsSrc.match(/\d+/g);
+            let apiParams = {
+                wid: a[0],
+                uiconfId: a[2],
+                refList: []
+            };
+
+            domList.each(function () {
+                let attr = isNew ?
+                    $(this).find('a').attr('href').match('.*/(.*-.*)')[1] :
+                    $(this).attr('data-clip-xmlid');
+                apiParams.refList.push(attr.replace('/', '-'));
+            });
+
+            getVideoForRefList(apiParams, domList);
+        }
+    }
+}();
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-(function() {
+var sboModule = sboModule || {};
+
+(function () {
+    let startNew = function () {
+
+        let techbusBaseUrl = 'https://techbus.safaribooksonline.com/';
+        let courseId = window.location.pathname.match('/([0-9]+)/?')[1];
+
+        $.get(techbusBaseUrl + courseId).always(function (html) {
+            sboModule.crawlerService.crawl($('.TableOfContents-TOCPart-Rl-Yx > ol > li'), html);
+        });
+    };
+
+    let startTechbus = function () {
+        setTimeout(function () {
+            sboModule.crawlerService.crawl($('a[data-clip-ref]'));
+        }, 5000);
+    };
+
     if (window.location.hostname.indexOf('www.safaribooksonline.com') >= 0) {
         startNew();
     } else {
